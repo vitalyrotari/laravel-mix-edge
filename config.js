7ff52b840709cb07956d0f4a2cd650c8134b3c7b
Vitaly Rotari <cky.adc@gmail.com>
2018-05-26 01:23:34 +0300

    add config file

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  loaderOptions: {
+    sourceExtension: '.edge',
+    outputExtension: '.html',
+    seeds: null,
+    presenters: null,
+  },
+  pluginOptions: {
+    edge: {},
+    global: {},
+    locals: {},
+  },
+};
